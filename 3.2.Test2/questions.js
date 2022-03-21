e9d63df0af657997abe6e8cddeb513de98737fcf
eliseprts <elisepourtois.pro@gmail.com>
2022-03-21 10:57:03 +0100

    upload work
    21/3 am jasmine 2

--- 3.2.Test2/questions.js
+++ 3.2.Test2/questions.js
@@ -1,43 +1,53 @@
-let CreationTableauLangages =  () => {
+const langages = ['Html', 'CSS', 'Java', 'PHP']
+const nombres = [0, 1, 2, 3, 4, 5]
 
+let CreationTableauLangages = () => {
+    return langages
 }
 
-let CreationTableauNombres =  () => {
-
+let CreationTableauNombres = () => {
+    return nombres
 }
 
-let RemplacementElement =  (langages) => {
-
+let RemplacementElement = (langages) => {
+    langages[2] = 'Javascript'
+    return langages
 }
 
-let AjoutElementLangages =  (langages) => {
-
+let AjoutElementLangages = (langages) => {
+    langages.push('Ruby', 'Python')
+    return langages
 }
 
-let AjoutElementNombres =  (nombres) => {
-
+let AjoutElementNombres = (nombres) => {
+    nombres.unshift(-2, -1)
+    return nombres
 }
 
-let SuppressionPremierElement =  (langages) => {
-
+let SuppressionPremierElement = (langages) => {
+    langages.shift('Html')
+    return langages
 }
 
-let SuppressionDernierElement =  (langages) => {
-
+let SuppressionDernierElement = (langages) => {
+    langages.pop()
+    return langages
 }
 
-let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
-
+let ConversionChaineTableau = (reseaux_sociaux_chaine) => {
+    const reseaux_sociaux = reseaux_sociaux_chaine.split(',')
+    return reseaux_sociaux
 }
 
-let ConversionTableauChaine =  (langages) => {
-
+let ConversionTableauChaine = (langages) => {
+    const langages_chaine = langages.toString(',')
+    return langages_chaine
 }
 
-let TriTableau =  (reseaux_sociaux) => {
-
+let TriTableau = (reseaux_sociaux) => {
+    return reseaux_sociaux.sort()
 }
 
-let InversionTableau =  (langages) =>{
-
+let InversionTableau = (langages) => {
+    return langages.reverse()
 }
